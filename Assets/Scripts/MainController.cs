9d6932bb72424728860c2c7c00b589422c5bd564
DESKTOP-2350VGN\tales <asamasou913@gmail.com>
2017-04-21 04:41:52 +0900

    うｐ

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour {
+
+    [SerializeField]
+    int count;//クリック数.
+    [SerializeField]
+    Text text;
+    [SerializeField]
+    Text countText;
+
+    float time;//ゲーム開始からの時間.
+    bool isStart = true;
+
+    /// <summary>
+    /// よーいドン、の準備.
+    /// 格ゲーならここでライフバーを出したり、戦う前のアレコレをする.
+    /// </summary>
+    /// <returns></returns>
+	IEnumerator Start () {
+        text.text = "";
+        countText.text = "";
+        yield return new WaitForSeconds(1);
+        text.text = "連打";
+        for (int i = 0; i < 3; i++) {
+            text.text += "・";
+            yield return new WaitForSeconds(0.3f);
+        }
+        text.text = "開始!!";
+        yield return new WaitForSeconds(1);
+        isStart = false;
+	}
+	
+	/// <summary>
+    /// ゲームのメイン部分.
+    /// </summary>
+	void Update () {
+        if (!isStart) {
+            if (time <= 20) {
+                countText.text = "COUNT:" + count;
+                text.text = "連打しろ!!";
+                if (Input.GetMouseButtonDown(0) || Input.anyKeyDown) {
+                    count++;
+                }
+                time += Time.deltaTime;
+            } else {
+                text.text = "RESULT : ";
+                if (count < 10) {
+                    text.text += "ミジンコ級";
+                } else if(count < 20) {
+                    text.text += "アリ級";
+                } else if(count < 40){
+                    text.text += "イヌ級";
+                } else if(count < 60){
+                    text.text += "素人級";
+                } else if(count < 80){
+                    text.text += "玄人級";
+                } else if(count < 100){
+                    text.text += "達人級";
+                } else{
+                    text.text += "伝説級";
+                }
+
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;//Text型を扱えるようにする.
+
+/* タイトルからメインのシーンに移動させたいだけ.
+ * ちょっとカッコつけてる.
+ */
+public class TitleController : MonoBehaviour {
+
+    public Text presskeyText;//テキストをエディタ上で取得する.
+
+	// １フレーム更新処理をこのUpdateメソッドという｛｝の中で行う.
+    //（フレームレートは【1秒間に画面が何回更新されるか】のことで、その処理1回ぶんをここで行っている）
+	void Update () {
+        //何かしらのキーが押されたとき.
+        if (Input.anyKeyDown) {
+            StartCoroutine("LoadScene");//LoadSceneのコルーチンメソッドを呼ぶ.
+        }
+	}
+
+    //IEnumratorにすると、コルーチンメソッドになる.
+    //コルーチンメソッドは、yield returnを扱えて、待つ処理ができる.（普通はメソッドが実行されたら一瞬）
+    IEnumerator LoadScene() {
+        for (int i = 0; i < 5; i++) {
+            presskeyText.enabled = !presskeyText.enabled;//現在のテキスト表示状態の反対.真なら偽、偽なら真になる.
+            yield return new WaitForSeconds(0.1f);//0.1秒待つ.
+        }
+        Application.LoadLevel("Main");//Mainという名前のシーンに切り替える.
+    }
+}
